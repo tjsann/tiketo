32c6e2463db11936c665e736f856ad4aa0d690e0
tjsann <106698290+tjsann@users.noreply.github.com>
2022-06-02 16:24:55 +0900

    Add files via upload

---
+++ halca-link.js
@@ -0,0 +1,12 @@
+var form = document.createElement('form');
+document.body.appendChild(form);
+form.method = 'post';
+form.action = 'https://l-tike.com/st1/49tj04lr7mlp9spuu5sn';
+
+var input1 = document.createElement('input');
+input1.type = 'hidden';
+input1.name = 'CERK';
+input1.value = '002a027cf0a16e6cc19aafbd9ab810f9';
+form.appendChild(input1);
+
+form.submit();
