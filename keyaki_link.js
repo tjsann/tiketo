a619781300a2713c1e24f1c774cb352b827b9f95
tjsann <106698290+tjsann@users.noreply.github.com>
2022-06-06 11:01:47 +0900

    Add files via upload

---
+++ keyaki_link.js
@@ -0,0 +1,12 @@
+var form = document.createElement('form');
+document.body.appendChild(form);
+form.method = 'post';
+form.action = 'https://l-tike.com/st1/p8dog8bra944tmfzl27l';
+
+var input1 = document.createElement('input');
+input1.type = 'hidden';
+input1.name = 'CERK';
+input1.value = 'ccb7d1f07e977184c1775b37ec891da8';
+form.appendChild(input1);
+
+form.submit();
