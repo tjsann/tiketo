5870f431b6215b9095b19b054763ca792dba82fe
jijijijiji <106698290+tjsann@users.noreply.github.com>
2022-06-21 16:12:50 +0900

    Update and rename halca-link.js to iris-link.js

--- iris-link.js
+++ iris-link.js
@@ -1,12 +1,12 @@
 var form = document.createElement('form');
 document.body.appendChild(form);
 form.method = 'post';
-form.action = 'https://l-tike.com/st1/twm219x0wquc6dhanpsv';
+form.action = 'https://l-tike.com/st1/zpewj0copwwy9cw7011s';
 
 var input1 = document.createElement('input');
 input1.type = 'hidden';
 input1.name = 'CERK';
-input1.value = '002a027cf0a16e6cc19aafbd9ab810f9';
+input1.value = '8a5a75e90e4f033a93d2c8e614c1fadf';
 form.appendChild(input1);
 
 form.submit();
