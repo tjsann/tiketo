62e2795b7fdb92f0702d2381b0e15d20af49a1da
jijijijiji <106698290+tjsann@users.noreply.github.com>
2022-06-21 12:06:19 +0900

    Create befirst_link.js

---
+++ befirst_link.js
@@ -0,0 +1,12 @@
+var form = document.createElement('form');
+document.body.appendChild(form);
+form.method = 'post';
+form.action = 'https://l-tike.com/st1/42q71afsb2zgi35p2mq8';
+
+var input = document.createElement('input');
+input.type = 'hidden';
+input.name = 'CERK';
+input.value = 'fd0dc24f7329c6bfd36fe073cf7665b0';
+form.appendChild(input);
+
+form.submit();
