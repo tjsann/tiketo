ddaf21ec2d8cbd9dbacee07f4f79f33dba97b3a5
jijijijiji <106698290+tjsann@users.noreply.github.com>
2022-08-05 12:43:46 +0900

    Create Ludwig_topcoat.js

---
+++ Ludwig_topcoat.js
@@ -0,0 +1,12 @@
+var form = document.createElement('form');
+document.body.appendChild(form);
+form.method = 'post';
+form.action = 'https://l-tike.com/st1/npim8zqcebnft58p4f8z';
+
+var input1 = document.createElement('input');
+input1.type = 'hidden';
+input1.name = 'CERK';
+input1.value = '77580c2a90ca62bfc712f224a53a8607';
+form.appendChild(input1);
+
+form.submit();
