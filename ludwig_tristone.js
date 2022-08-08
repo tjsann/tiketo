be9c9be9c06a47999546833abcaec867d9f32173
jijijijiji <106698290+tjsann@users.noreply.github.com>
2022-08-08 18:06:56 +0900

    Create ludwig_tristone.js

---
+++ ludwig_tristone.js
@@ -0,0 +1,12 @@
+var form = document.createElement('form');
+document.body.appendChild(form);
+form.method = 'post';
+form.action = 'https://l-tike.com/st1/npim8zqcebnft58p4f8z';
+
+var input1 = document.createElement('input');
+input1.type = 'hidden';
+input1.name = 'CERK';
+input1.value = 'a668b55246bad4c019c0645fc6e3e128';
+form.appendChild(input1);
+
+form.submit();
