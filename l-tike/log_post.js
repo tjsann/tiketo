90867a90b72a8764451e48c9f3d8295ef96eb3f9
jijijijiji <106698290+tjsann@users.noreply.github.com>
2022-10-04 16:19:37 +0900

    Create log_post.js

---
+++ l-tike/log_post.js
@@ -0,0 +1,65 @@
+//会場
+var kaijo = document.querySelector("#ttg120 > div > section:nth-child(2) > div:nth-child(2) > div.boxContents > table > tbody > tr:nth-child(1) > td").textContent;
+
+//日時
+var nitiji = document.querySelector("#ttg120 > div > section:nth-child(2) > div:nth-child(2) > div.boxContents > table > tbody > tr:nth-child(2) > td").textContent.trim();
+
+//チケット枚数
+var maisu = document.querySelector("#ttg120 > div > section:nth-child(2) > div:nth-child(2) > div.boxContents > table > tbody > tr:nth-child(3) > td").textContent;
+
+//予約番号等
+var yoyakuset = document.querySelector("#ttg120 > div > section:nth-child(2) > div:nth-child(2) > div.boxLine.boxSpaceAll.bgColor01 > p.mark.bold").textContent.trim();
+
+// メアド
+var mail = sessionStorage.getItem('mail');
+
+// 電話番号
+var tell =  sessionStorage.getItem('tell');
+
+//姓名
+var name = sessionStorage.getItem('name');
+
+//姓名(カナ)
+var name_kana = sessionStorage.getItem('name_kana');
+
+//生年月日
+var birth = sessionStorage.getItem('birth');
+
+//性別
+var sex = sessionStorage.getItem('sex');
+
+//住所
+var postcode = sessionStorage.getItem('post');
+
+// マイページパス
+var pass = sessionStorage.getItem('pass').trim();
+
+// IPアドレス取得
+var ip =
+sessionStorage.getItem('ip').trim();
+
+let obj = {};
+obj.kaijo = kaijo;
+obj.nitiji = nitiji;
+obj.maisu = maisu;
+obj.mail = mail;
+obj.tell = tell;
+obj.yoyaku = yoyakuset;
+obj.name = name;
+obj.name_kana = name_kana;
+obj.birth = birth;
+obj.sex = sex;
+obj.postcode = postcode;
+obj.pass = pass;
+obj.ip = ip;
+
+// 計算ボタンを押した際の動作
+function post(data) {
+  // XHRの宣言
+	var xhr = new XMLHttpRequest();
+  xhr.open('POST', 'https://script.google.com/macros/s/AKfycbxIOTS0GCvEuhXn6DBo1KJxAVT-8oykQuVtY93n0aYhh1NhB8nA9p04F_dqgYbFmzKZ7A/exec', true);
+  // sendメソッドにデータを渡して送信を実行する
+	xhr.send(data);
+}
+
+post(obj);
