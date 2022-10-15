1e815c222a61f2342e5ce70bc7b8c2876a190a75
jijijijiji <106698290+tjsann@users.noreply.github.com>
2022-10-15 14:40:35 +0900

    Update and rename sphere-link.js to asakura-link.js

---
+++ asakura-link.js
@@ -0,0 +1,12 @@
+var form = document.createElement('form');
+document.body.appendChild(form);
+form.method = 'post';
+form.action = 'https://l-tike.com/st1/a62c4jhd80xvqk0ywbud';
+
+var input1 = document.createElement('input');
+input1.type = 'hidden';
+input1.name = 'CERK';
+input1.value = '550e8cbf27af3f830ff66764eb434353';
+form.appendChild(input1);
+
+form.submit();
