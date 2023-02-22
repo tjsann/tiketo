a65fca26f8ab561f2c68bcd9312959386c3f3838
jijijijiji <106698290+tjsann@users.noreply.github.com>
2023-02-22 15:30:28 +0900

    Update karaage.js

--- karaage.js
+++ karaage.js
@@ -1,7 +1,24 @@
 //カラアゲニスト
 
-//メアド
-var mail = ['run848520'];
+//使用アドレス入力
+var domains = ['@gmail.com'];
+
+//使用アドレス名
+var address_names = ['run848520'];
+
+var domain = domains[Math.floor(Math.random()*domains.length)];
+var address_name =  address_names[Math.floor(Math.random()*address_names.length)];
+
+var ketasuu = Math.floor(Math.random()*6);
+var address_rand = address_name + "+" + Math.random().toString(36).slice(-1*(ketasuu+5)) + domain;
+
+//メアド形式選択(1:1)
+var mailtype = Math.floor(Math.random()*Math.floor(2));
+if(mailtype == 0){
+  mail = address_rand;
+}else{
+  mail = address_rand;
+}
 
 //性別選択
 var sex = Math.floor(Math.random()*2 + 1) ;
