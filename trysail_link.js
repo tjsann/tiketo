3f92215a447f1066e8229591780a79973a97f4ac
jijijijiji <106698290+tjsann@users.noreply.github.com>
2023-04-18 22:44:16 +0900

    Create trysail_link.js

---
+++ trysail_link.js
@@ -0,0 +1,12 @@
+var form = document.createElement('form');
+document.body.appendChild(form);
+form.method = 'post';
+form.action = 'https://l-tike.com/st1/zupgx3w37ab4ohegqcng';
+
+var input1 = document.createElement('input');
+input1.type = 'hidden';
+input1.name = 'CERK';
+input1.value = '6c37e161f169b572da0975f6d12fbe86';
+form.appendChild(input1);
+
+form.submit();
