bfd03eef5183feb0027bb23a3c9aa6d9c79385e9
jijijijiji <106698290+tjsann@users.noreply.github.com>
2023-07-28 13:52:57 +0900

    Update and rename trysail_link.js to hinako_t_link.js

--- hinako_t_link.js
+++ hinako_t_link.js
@@ -1,12 +1,12 @@
 var form = document.createElement('form');
 document.body.appendChild(form);
 form.method = 'post';
-form.action = 'https://l-tike.com/st1/zupgx3w37ab4ohegqcng';
+form.action = 'https://va.pia.jp/haroldandmaude23sh/entrance.jsp';
 
 var input1 = document.createElement('input');
 input1.type = 'hidden';
-input1.name = 'CERK';
-input1.value = '6c37e161f169b572da0975f6d12fbe86';
+input1.name = 'acpt_no';
+input1.value = '2498939091517162';
 form.appendChild(input1);
 
 form.submit();
