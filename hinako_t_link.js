02b8b76ab7ab0c2c5151576f2ad3c039fd219df4
jijijijiji <106698290+tjsann@users.noreply.github.com>
2023-07-28 13:54:19 +0900

    Update hinako_t_link.js

--- hinako_t_link.js
+++ hinako_t_link.js
@@ -9,4 +9,10 @@ input1.name = 'acpt_no';
 input1.value = '2498939091517162';
 form.appendChild(input1);
 
+var input2 = document.createElement('input');
+input2.type = 'hidden';
+input2.name = 'slcd';
+input2.value = 'VL793';
+form.appendChild(input2);
+
 form.submit();
