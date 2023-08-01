e061494da93136d5dd7cd30650c61052b52063fb
jijijijiji <106698290+tjsann@users.noreply.github.com>
2023-08-02 00:22:32 +0900

    Update and rename iris-link.js to emori_link.js

--- emori_link.js
+++ emori_link.js
@@ -1,12 +1,12 @@
 var form = document.createElement('form');
 document.body.appendChild(form);
 form.method = 'post';
-form.action = 'https://l-tike.com/st1/zpewj0copwwy9cw7011s';
+form.action = 'https://www.funity.jp/tickets/ayaemori/show/EA1stfan24/1/3';
 
 var input1 = document.createElement('input');
 input1.type = 'hidden';
-input1.name = 'CERK';
-input1.value = '8a5a75e90e4f033a93d2c8e614c1fadf';
+input1.name = 'smd';
+input1.value = 'qOV54tiT7Y';
 form.appendChild(input1);
 
 form.submit();
