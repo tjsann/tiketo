c62ed97cc74b83679c93b0b855442f0a82dc9704
jijijijiji <106698290+tjsann@users.noreply.github.com>
2023-10-27 13:35:41 +0900

    Update and rename keyaki_link.js to nintama_link.js

--- nintama_link.js
+++ nintama_link.js
@@ -1,12 +1,12 @@
 var form = document.createElement('form');
 document.body.appendChild(form);
 form.method = 'post';
-form.action = 'https://l-tike.com/st1/p8dog8bra944tmfzl27l';
+form.action = 'https://eplus.jp/sf/detail/0348300008?P6=777';
 
 var input1 = document.createElement('input');
 input1.type = 'hidden';
-input1.name = 'CERK';
-input1.value = 'ccb7d1f07e977184c1775b37ec891da8';
+input1.name = 'keyword';
+input1.value = '11400';
 form.appendChild(input1);
 
 form.submit();
