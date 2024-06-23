f98433a8d8bc80c7dbf9d56b81dba94f4bcc67ec
jijijijiji <106698290+tjsann@users.noreply.github.com>
2024-06-23 15:01:01 +0900

    Update siina_d_try_link.js

--- hinako_o_link.js
+++ hinako_o_link.js
@@ -1,18 +1,12 @@
 var form = document.createElement('form');
 document.body.appendChild(form);
 form.method = 'post';
-form.action = 'https://va.pia.jp/haroldandmaude23sh/entrance.jsp';
+form.action = 'https://ticket.ponycanyon.co.jp/FV4MjbxJ79K6Ydm-p';
 
 var input1 = document.createElement('input');
 input1.type = 'hidden';
-input1.name = 'acpt_no';
-input1.value = '9935786684147177';
+input1.name = 'ck';
+input1.value = 'x2SDdRluespP9MfaA';
 form.appendChild(input1);
-
-var input2 = document.createElement('input');
-input2.type = 'hidden';
-input2.name = 'slcd';
-input2.value = 'VL794';
-form.appendChild(input2);
 
 form.submit();
