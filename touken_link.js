ac20d2e1551abebb9980e7de070aca6767e82a20
jijijijiji <106698290+tjsann@users.noreply.github.com>
2025-04-08 20:31:30 +0900

    Update and rename befirst_link.js to touken_link.js

--- touken_link.js
+++ touken_link.js
@@ -1,12 +1,12 @@
 var form = document.createElement('form');
 document.body.appendChild(form);
 form.method = 'post';
-form.action = 'https://l-tike.com/st1/42q71afsb2zgi35p2mq8';
+form.action = 'https://l-tike.com/st1/xgo0y7fb2c1d1ldd6ttz';
 
 var input = document.createElement('input');
 input.type = 'hidden';
 input.name = 'CERK';
-input.value = 'fd0dc24f7329c6bfd36fe073cf7665b0';
+input.value = '2cc28e1649ef40d7ed67fa2917a744c0';
 form.appendChild(input);
 
 form.submit();
