2df6f9dc67000115f7076f532e0d961615e4d233
jijijijiji <106698290+tjsann@users.noreply.github.com>
2025-05-06 23:57:18 +0900

    Update mokatan_link.js

--- mokatan_link.js
+++ mokatan_link.js
@@ -3,17 +3,21 @@ document.body.appendChild(form);
 form.method = 'post';
 form.action = 'https://ticket.skiyaki.tokyo/events/mokahinata/token_restriction';
 
+// tk（トークン）
 var input1 = document.createElement('input');
 input1.type = 'hidden';
 input1.name = 'tk';
 input1.value = 'ojez36aDhRxmrsex/dcFAo2nHaT3xH4yva0C8atg9VBSMerKp/lxU5MUgSgzgTNzo5w/hXpWkDfHlbO/kwy7PA==';
 form.appendChild(input1);
 
+// t（動的タイムスタンプ）
+var now = Math.floor(new Date().getTime() / 1000);
 var input2 = document.createElement('input');
 input2.type = 'hidden';
 input2.name = 't';
 input2.id = 'st-timestamp-5280';
-input2.value = 1746542690
+input2.value = now;
 form.appendChild(input2);
 
+// 送信
 form.submit();
